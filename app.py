d975577b8fe667b31b3c61d8b4b17090d8ccd4ac
Samarth Manjunath Hathwar <samarthmhathwar@Samarths-Mac-mini.local>
2026-03-26 23:00:47 +0100

    Implemented CLIP based image understanding with zero-shot classificaion

--- app.py
+++ app.py
@@ -1,25 +1,43 @@
 from PIL import Image
+import torch
+import clip
 
-def load_image(path):
-    try: 
-        image = Image.open(path)
-        return image
-    except Exception as e:
-        print(f"Error loading image: {e}")
-        return None
+def load_model():
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model, preprocess = clip.load("ViT-B/32", device = device)
+    return model, preprocess, device
+
+def process_image(image_path, preprocess, device):
+    image = preprocess(Image.open(image_path)).unsqueeze(0).to(device)
+    return image
+
 
 def main():
     image_path = "images/test.jpg"
-    image = load_image(image_path)
+    #image = load_image(image_path)
 
-    if image:
-        print("Image loaded successfully!")
-        print(f"Format : {image.format}")
-        print(f"Size : {image.size}")
-        print(f"Mode : {image.mode}")
-        image.show() # Opens image
-    else: 
-        print("Failed to load image")
+    print("Loading CLIP model..")
+    model, preprocess, device = load_model()
+
+    print("Preprocessing image..")
+    image = process_image(image_path, preprocess,device)
+
+    labels = [
+        "a child",
+        "a camel",
+        "a desert",
+        "a person laughing",
+        "an animal"
+    ]
+    text = clip.tokenize(labels).to(device)
+
+    with torch.no_grad():
+        logits_per_image, _ = model(image,text)
+        probs = logits_per_image.softmax(dim=-1).cpu().numpy()
+    print("\n Predictions:")
+    for labels, prob in zip(labels, probs[0]):
+        print(f"{labels}: {prob:.4f}")
+
 
 if __name__ == "__main__":
     main()
