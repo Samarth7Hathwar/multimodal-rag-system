0ddad8c3ea5d95c215693c64a7eb23ff277bee1a
Samarth Manjunath Hathwar <samarthmhathwar@Samarths-Mac-mini.local>
2026-03-27 22:10:15 +0100

    Intergrated FAISS with normalized embeddings for accurate retrieval

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 from PIL import Image
 import torch
 import clip
+import faiss
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+
+def build_faiss_index(text_features):
+    # Convert to numpy
+    vectors = text_features.cpu().numpy()
+
+    dim = vectors.shape[1] # embedding size(512)
+
+    index = faiss.IndexFlatL2(dim)
+    index.add(vectors)
+
+    return index
 
 def load_model():
     device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -16,32 +30,54 @@ def encode_texts(docs, model, device):
     text_tokens = clip.tokenize(docs).to(device)
     with torch.no_grad():
         text_features = model.encode_text(text_tokens)
+    text_features = text_features / text_features.norm(dim=-1, keepdim = True)
     return text_features
 
 def encode_image(image_path, model, preprocess, device):
     image = preprocess(Image.open(image_path)).unsqueeze(0).to(device)
     with torch.no_grad():
         image_features = model.encode_image(image)
+    image_features = image_features / image_features.norm(dim=-1, keepdim = True)
     return image_features
     
-def retrieve(image_features, text_features, docs):
-    similarities = (image_features @ text_features.T).squeeze(0)
-    best_idx = similarities.argmax().item()
-    return docs[best_idx], similarities[best_idx].item()
+def retrieve_faiss(image_features, index, docs):
+    query = image_features.cpu().numpy()
+
+    D, I = index.search(query, k=1) # k=1 -> best match
+
+    best_idx = I[0][0]
+    score = D[0][0]
+
+    return docs[best_idx], score
 
 def main():
+    print("Loading model..")
     model, preprocess, device = load_model()
 
+    print("Loading documents..")
     docs = load_documents("docs/data.txt")
+
+    print("Encoding text..")
     text_features = encode_texts(docs, model, device)
+
+    print("Encoding image..")
     image_path = "images/test.jpg"    
     image_features = encode_image(image_path, model , preprocess, device)
 
-    best_doc, score = retrieve(image_features, text_features, docs)
-    print("Most relevant documents: ")
+    # best_doc, score = retrieve(image_features, text_features, docs)
+    # build index
+    print("Building FAISS index..")
+    index = build_faiss_index(text_features)
+
+    print("Retrieving..")
+    best_doc, score = retrieve_faiss(image_features, index , docs)
+
+    print("Most relevant document: ")
     print(best_doc)
-    print(f"\nSimilarity Score: {score:.4f}")
+    #print(f"\nSimilarity Score: {score:.4f}")
+    print(f"\nDistance Score (lower is better): {score:.4f}")
 
 
 if __name__ == "__main__":
     main()
+
